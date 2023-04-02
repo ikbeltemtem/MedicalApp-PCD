135e5847f0b0469b9b38dc6e7af5058570fdacb3
ikbeltemtem <ikbel.temtem@ensi-uma.tn>
2023-04-02 20:39:18 +0100

    ajout therapie cors fixed +path not yet

---
+++ backend/project/src/main/java/com/medical/project/Entity/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.medical.project.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    String password;
+}
---
+++ backend/project/src/main/java/com/medical/project/Entity/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.medical.project.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+
+}
